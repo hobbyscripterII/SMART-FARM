4b7c2753088bb131930fcf7e3888ca8da82881f7
hobbyscripterII <wruomma2@naver.com>
2025-03-15 20:15:13 +0900

    [2025-03-15] UPDATE - 차트 표출 작업 - localStorage 지역 코드 누적 추가 버그 수정

--- src/main/resources/static/js/cmmn.js
+++ src/main/resources/static/js/cmmn.js
@@ -5,148 +5,27 @@ $(document).ready(() => {
     // 현재 시간 표출
     yyyyMMddhhmmss();
     setInterval(yyyyMMddhhmmss, 1000);
-
-    // ajax 호출
-
-    // serialNumber에 저장된 정보 확인
-
-    // [참고] API 연결 전까지는 아래 더미 데이터로 동적 처리
-
-    let user = null;
-    const localStorageUser = localStorage.getItem('user');
-    const params = new URLSearchParams(window.location.search);
-    const queryStringUser = params.get('user');
-
-    if(queryStringUser) {
-        user = queryStringUser;
-        localStorage.setItem('user', user);
-    } else if(localStorageUser) {
-        user = localStorageUser;
-        localStorage.setItem('user', user);
-    } else {
-        // location.href = 'error.html';
-        return false;
-    }
-
-    const userData = getUserDummyData(user);
-
-    // 작물 기본 정보 세팅
-    const userName = userData.userName;
-    const profileImg = userData.profileImg;
-    const plantList = userData.plantList;
-    const role = userData.role;
-
-    // nav 세팅
-    // 작물 목록 표출
-    const navbarWrap = $('.navbar-wrap');
-    let el = `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>작물 목록</span></li>`;
-    
-    plantList.forEach((item) => {
-        const plantName = item;
-        el += `<li class="sub-plant" onclick="chagePlant('${plantName}')">${plantName}</li>`;
-    });
-    
-    // 온습도 변화량 조회
-    el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>온습도 변화량 조회</span></li>`;
-    el += `<li class="sub-plant">변화 시점 기기 및 모듈 상태 조회</li>`;
-    el += `<li class="sub-plant">IP 카메라 이미지 조회</li>`;
-    // el += `<li class="sub-plant" onclick="chagePlant('${plantName}')">${plantName}</li>`;
-    
-    // 관수 시설 상태 변화량 조회
-    el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>관수 시설 상태 변화량 조회</span></li>`;
-    el += `<li class="sub-plant">물탱크 수위계 조회</li>`;
-    el += `<li class="sub-plant">관수량 측정계 조회</li>`;
-
-    // 시간별 변화량 조회
-    el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>시간별 변화량 조회</span></li>`;
-    el += `<li class="sub-plant">개폐량 조회</li>`;
-    el += `<li class="sub-plant">일조량 조회</li>`;
-
-    // 양액용 모듈 관리
-    el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>양액용 모듈 관리</span></li>`;
-    el += `<li class="sub-plant">양액 투입량 조회</li>`;
-    el += `<li class="sub-plant">양액 투입량 수정</li>`;
-    el += `<li class="sub-plant">양액 종류 조회</li>`;
-    el += `<li class="sub-plant">양액 종류 수정</li>`;
-
-    // 작물 관리
-    el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>작물 관리</span></li>`;
-    el += `<li class="sub-plant">작물 정보 조회</li>`;
-    el += `<li class="sub-plant">작물 정보 수정</li>`;
-    el += `<li class="sub-plant">작물 성장상태 조회</li>`;
-
-    if(role == 'ROOT' || role == 'MANAGER') {
-        // 지역 관리
-        el += `<li class="main-plant"><img src="https://img.icons8.com/?size=20&id=88608&format=png"><span>지역 관리</span></li>`;
-        el += `<li class="sub-plant"><a href="region.html">지역 조회</a></li>`;
-        /* el += `<li class="sub-plant">지역 수정</li>`; */
-    }
-
-    // navbarWrap.append(el);
-
-    // 첫 화면에 보여줄 센서 데이터 및 차트
-    const defaultPlant = plantList[0]; // 첫번째 작물
-    // chagePlant(defaultPlant);
-	
-	// const regionCds = "[[${#authentication.principal.regionCds}]]";
-	// console.log('regionCds = ', regionCds);
 });
 
 function yyyyMMdd() {
     let currentDate = new Date();
-    let year = currentDate.getFullYear();
-    let month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    let day = currentDate.getDate().toString().padStart(2, '0');
-    let resultDate = `${year}-${month}-${day}`;
+    let year        = currentDate.getFullYear();
+    let month       = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    let day         = currentDate.getDate().toString().padStart(2, '0');
+    let resultDate  = `${year}-${month}-${day}`;
 
 	return resultDate;
 }
 
 function yyyyMMddhhmmss() {
     let currentDate = new Date();
-
-    let year = currentDate.getFullYear();
-    let month = (currentDate.getMonth() + 1).toString().padStart(2, '0');
-    let day = currentDate.getDate().toString().padStart(2, '0');
-    let hours = currentDate.getHours().toString().padStart(2, '0');
-    let minutes = currentDate.getMinutes().toString().padStart(2, '0');
-    let seconds = currentDate.getSeconds().toString().padStart(2, '0');
-    let resultDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+    let year        = currentDate.getFullYear();
+    let month       = (currentDate.getMonth() + 1).toString().padStart(2, '0');
+    let day         = currentDate.getDate().toString().padStart(2, '0');
+    let hours       = currentDate.getHours().toString().padStart(2, '0');
+    let minutes     = currentDate.getMinutes().toString().padStart(2, '0');
+    let seconds     = currentDate.getSeconds().toString().padStart(2, '0');
+    let resultDate  = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
     $('#time').html(resultDate);
-}
-
-function getUserDummyData(user) {
-    let userData = {};
-
-    switch(user) {
-        case 's' :
-            userData = {
-                userName : '정성운',
-                profileImg : '/img/img_profile_1.jpg',
-                plantList : ['방울 토마토', '고수'],
-                role : 'ROOT'
-            };
-            break;
-            
-        case 'j' :
-            userData = {
-                userName : '이주영',
-                profileImg : 'img/img_profile_2.jpg',
-                plantList : ['딸기', '바질'],
-                role : 'MANAGER'
-            };
-            break;
-
-        case 'g' :
-            userData = {
-                userName : '게스트',
-                profileImg : 'img/img_profile_2.jpg',
-                plantList : ['딸기', '바질'],
-                role : 'GUEST'
-            };
-            break;
-    }
-
-    return userData;
 }
--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
-	// 클릭 메뉴 강조 효과
-	changeNavActive();
+	let regionCode = null;
 	
-	// sub-nav 클릭 시 a 태그 트리거
-	subNavClickEvent();
+	changeNavActive();  // 클릭 메뉴 강조 효과
+	subNavClickEvent(); // sub-nav 클릭 시 a 태그 트리거
 	
+	const houseListEl   = $('#house-list');
+	const regionCodeMap = JSON.parse(localStorage.getItem('regionCodeMap')); // 로컬 스토리지에 담긴 지역 코드 확인
+	
+	if(regionCodeMap) {
+		const jsonParseMap = new Map(Object.entries(regionCodeMap));
+		regionCode         = jsonParseMap.get('regionCode');
+		const regionName   = jsonParseMap.get('regionName');
+		const optionEl     = `<option value=${regionCode}>${regionName}</option>`;
+
+		houseListEl.append(optionEl);
+	}
+
 	const regionCodes = $('#region-cds').val() || '';
 	const dto = {"regionCodes" : regionCodes};
 	
 	if(regionCodes != '') {
-		// 작물 목록 세팅
 		$.ajax({
 		    type: 'GET',
 		    url: window.apiUrl + '/region',
@@ -18,125 +28,34 @@ $(document).ready(function() {
 		    contentType: 'application/json',
 		    success: (data) => {
 				const result = data.data;
-				const houseListEl = $('#house-list');
-				const firstRegionCd = result[0].regionCd;
 				
 				$.each(result, function(idx, item) {
-					const regionCd = item.regionCd;
-					const regionName = item.regionName;
-					const optionEl = `<option value=${regionCd}>${regionName}</option>`;
+					const regionCode = item.regionCd;
+					const regionName = item.regionName.replaceAll('_', ' ');
+					const optionEl   = `<option value=${regionCode}>${regionName}</option>`;
 					
 					houseListEl.append(optionEl);
 				});
-				
-				// 기본 정보 세팅
-				defaultSettings(firstRegionCd);
 		    },
-		    error: (x) => { console.log(x); }
+		    error: (x) => {
+				console.log(x);
+			}
 		});
 	}
 });
 
-function defaultSettings(regionCode) {
-	const dto = {"date" : yyyyMMdd(), "regionCode" : regionCode};
-	
-	// 센서 데이터 초기화
-	const sensorDataEl = $('.sensor-data');
-	sensorDataEl.html('');
-	
-	$.ajax({
-	    type: 'GET',
-	    url: window.apiUrl + '/weather',
-	    data: dto,
-	    dataType: 'json',
-	    contentType: 'application/json',
-	    success: (data) => {
-			const result = data.data;
-			// const sensorDefaultList = getSensorDefaultList();
-			let datasets = null;
-			const statusArr = [];
-			const temperatureArr = [];
-			const windPwrArr = [];
-			const windDirectionArr = [];
-			const updateDttmArr = [];
-			const humidityArr = [];
-			
-			$.each(result, function(idx, item) {
-				const status = item.status;
-				const temperature = item.temperature;
-				const windPwr = item.windPwr;
-				const windDirection = item.windDirection;
-				const updateDttm = item.updateDttm;
-				const humidity = item.humidity;
-				
-				statusArr.push(status);
-				temperatureArr.push(temperature);
-				windPwrArr.push(windPwr);
-				windDirectionArr.push(windDirection);
-				updateDttmArr.push(updateDttm);
-				humidityArr.push(humidity);
-			});
-			
-			datasets = `
-				[
-					{
-						"name": "상태",
-						"data": ${statusArr},
-						"unit": "",
-						"type": "line",
-						"valueDecimals": 1
-					},
-					{
-						"name": "온도",
-						"data": ${temperatureArr},
-						"unit": "°C",
-						"type": "line",
-						"valueDecimals": 1
-					},
-					{
-						"name": "풍속",
-						"data": ${windPwrArr},
-						"unit": "m/s",
-						"type": "line",
-						"valueDecimals": 1
-					},
-					{
-						"name": "풍향",
-						"data": ${windDirectionArr},
-						"unit": "",
-						"type": "line",
-						"valueDecimals": 1
-					},
-					{
-						"name": "습도",
-						"data": ${humidityArr},
-						"unit": "%",
-						"type": "line",
-						"valueDecimals": 1
-					}
-				]
-			`;
-			
-			// console.log('datasets = ', datasets);
-			
-	    },
-	    error: (x) => { console.log(x); }
-	})
-}
 
-function getChartDataInfo(type) {
-	let chartInfoClass = {};
-}
-
+// sub-nav 클릭 시 a 태그 트리거 이벤트
 function subNavClickEvent() {
 	$(document).on('click', '.sub-nav', function() {
 		window.location = $(this).find('a').attr('href');
 	})
 }
 
+// 클릭 메뉴 강조 효과 이벤트
 function changeNavActive() {
 	const currentPath = window.location.pathname;
-    const subPlantEl = $('.sub-nav');
+    const subPlantEl  = $('.sub-nav');
 	
     subPlantEl.each((idx, el) => {
 		const elHref = $(el).find('a').attr('href');
@@ -150,13 +69,24 @@ function changeNavActive() {
     });
 }
 
-function getSensorDefaultList() {
-	const sensorList = {
-	    'temp' : {name: '온도' , unit: '°C'},
-	    'humi' : {name: '습도' , unit: '%' },
-	    'open' : {name: '개폐량', unit: '%' },
-	    'light': {name: '일조량', unit: 'LX'}
-	};
+function chagePlant(el) {
+	localStorage.removeItem('regionCodeMap');
 	
-	return sensorList;
+	const regionCode    = ($(el).val()).trim();
+	const regionName    = ($(el).find(':selected').text()).trim();
+	const regionCodeMap = new Map();
+	
+	regionCodeMap.set('regionCode', regionCode);
+	regionCodeMap.set('regionName', regionName);
+	
+	localStorage.setItem('regionCodeMap', JSON.stringify(Object.fromEntries(regionCodeMap)));
+	
+	const currentUrl = window.location.pathname;
+	const targetUrl  = '/manager/home';
+	
+	// 차트 표출하는 페이지에 있을 경우에만 차트 정보 변경 이벤트 실행
+	if(currentUrl == targetUrl) {
+		getChart(regionCode);
+		getSensor();
+	}
 }
